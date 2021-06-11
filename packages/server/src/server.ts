51c3f72686f888f687dfd57f2ce443598a5f6606
pedromoraisf <pedro.morais1997@gmail.com>
2021-06-11 12:31:19 -0300

    temp(server): only for tests

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -7,6 +7,6 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World with workflow using Github Actions!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
